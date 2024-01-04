8d4b15e93a890ab036af8953c3dc00b8e5f71a3b
Sifa Nur Febriani <sifanurfebriani@gmail.com>
2024-01-04 09:32:26 +0700

    add home

--- Home/home.php
+++ Home/home.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="../home-style.css">
+    <link rel="stylesheet" href="home-style.css">
 
   </head>
   <body>
@@ -24,10 +24,10 @@
           <a class="nav-link active" aria-current="page" href="home.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="halaman/pil_penyakit.php">Data Penyakit</a>
+          <a class="nav-link" href="pil_penyakit.php">Data Penyakit</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="data_diagnosa.php">Data Gejala</a>
+          <a class="nav-link" href="pil_gejala.php">Data Gejala</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Diagnosa</a>
@@ -42,10 +42,14 @@
     <!-- navbar end -->
 
     <!-- header  -->
-    <section class="header align-items-center justify-content-center">
+    <section class="first align-items-center justify-content-center">
         <div class="container">
             <h1 class="display-1">Sistem Pakar</h1>
-            <h4>Sistem pakar adalah sistem informasi yang berisi pengetahuan seorang pakar sehingga dapat digunakan untuk konsultasi </h4>
+              <h6 class="mb-3">Sistem pakar (expert system) adalah sistem yang berusaha mengapdosi pengetahuan manusia ke komputer, agar komputer dapat menyelesaikan masalah seperti yang biasa dilakukan oleh para ahli. Sistem pakar yang baik dirancang agar dapat menyelesaikan suatu permasalahan tertentu dengan meniru kerja dari para ahli.</h6>
+              <h6 class="">
+              Infeksi menular seksual atau penyakit menular seksual adalah infeksi yang menular melalui hubungan intim. Penyakit ini umumnya ditandai dengan ruam atau lepuh, keputihan, dan nyeri di area kelamin. Ada banyak jenis penyakit menular seksual, di antaranya Penyakit Menular Seksual Jenis Hepatitis B, Genital Wans, Klamidia, Gonore, Sifilis, Herpes Genital.
+              </h6>
+            </div>
         </div>
     </section>
     <!-- header  end -->
